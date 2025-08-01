38bfba3ca2d9fc9cebaed1586580d7337f3a7f3c
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-01 11:55:44 +0700

    Berikut tahap tahapan untuk melakukkan logic auth dengan firebase : 1). Membuat server action untuk melakukkan proses sign-up dan melakukkan penyesuaian pada saat form disubmit jika type nya sign-up maka akan membuat akun || .....

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,41 @@
+"use server";
+
+// class import menggunakan : ctrl + .
+
+import {db} from "@/firebase/admin";
+
+export async function signUp(params: SignUpParams) {
+    const {uid, name, password, email} = params;
+
+    try {
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if(userRecord.exists) {
+            return {
+                success: false,
+                message: 'User sudah tersedia, silahkan sig-in'
+            }
+        }
+
+        await db.collection('user').doc(uid).set({
+            name: name,
+            email: email
+        })
+
+    } catch (e: any) {
+        console.error('Gagal membuat akun user', e);
+
+
+        if(e.code === 'auth/email-already-exists') {
+            return {
+                success: false,
+                message: 'Email yang anda gunakan sudah tersedia'
+            }
+        }
+        return {
+            success: false,
+            message: 'Gagal membuat akun user'
+        }
+
+    }
+}
