e41d359ec49cbab951e8a59ff4e743a87a82c96e
abbu solihin alhakim <alhakimabusolihin@gmail.com>
2025-08-01 12:03:40 +0700

    ... Melanjutkan tahapan auth || 2). Membuat session cookies : Karena firebase bersifat custom maka kita harus set auth nya menjadi 4 tahap yaitu --> 1. signup, 2. sessioncookies create, 3. login, 4. logout ||.....  <--

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,7 +2,10 @@
 
 // class import menggunakan : ctrl + .
 
-import {db} from "@/firebase/admin";
+import {auth, db} from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
     const {uid, name, password, email} = params;
@@ -38,4 +41,20 @@ export async function signUp(params: SignUpParams) {
         }
 
     }
+}
+
+export async function setSessionCookies(idToken: string) {
+    const cookieStore = await cookies();
+
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_WEEK + 1000
+    })
+
+    cookieStore.set('session', sessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        path: "/",
+        sameSite: "lax"
+    })
 }
